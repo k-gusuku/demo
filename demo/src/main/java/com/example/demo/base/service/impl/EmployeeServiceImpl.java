6ecd2573e3860e5108bc758c932d18dd8220dc47
oshiro <oshirokounosuke@gmail.com>
2025-03-27 21:33:38 +0900

    チェリーピック②

--- demo/src/main/java/com/example/demo/base/service/impl/EmployeeServiceImpl.java
+++ demo/src/main/java/com/example/demo/base/service/impl/EmployeeServiceImpl.java
@@ -26,6 +26,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Qualifier("EmployeeDaoImpl")
     // チェリーピック①
     private final EmployeeDao employeeDao;
+    // チェリーピック②
     private final EmployeeJdbc employeeJdbc;
     private final PasswordEncoder passwordEncoder;
 
